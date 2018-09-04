2465d85922e51ca61bec5037b0609517728b770d
wowdyaln <rogersmk@gmail.com.tw>
2018-09-04 11:25:25 +0800

    week1 hw5 done.

--- homeworks/week1/hw5.js
+++ homeworks/week1/hw5.js
@@ -1,7 +1,23 @@
-function join(str, concatStr) {
+function join(array, concatStr) {
+  let newStr = ""
+  let index = 0
 
+  for (let i=1; i <= array.length*2-1; i++){
+    if (i%2 !== 0){
+      newStr += array[index]
+      index++
+    }
+    if (i%2 === 0){
+      newStr += concatStr
+    }
+  }
+  return newStr
 }
 
 function repeat(str, times) {
-
+  let newStr = ""
+  for ( let i=0; i < times; i++){
+    newStr += str
+  }
+  return newStr
 }
