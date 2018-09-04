00e55ae88cfdf63e1cc3380b22d3c235e81c4b6c
wowdyaln <rogersmk@gmail.com.tw>
2018-09-04 11:00:57 +0800

    week1 hw3 done.

--- homeworks/week1/hw3.js
+++ homeworks/week1/hw3.js
@@ -1,3 +1,13 @@
-function reverse(str) {
+/*
+  1. 使用 .split("") 分割
+  2. 每個 element ，unshift 到另一個新的 array
+*/
 
+function reverse(str) {
+  let ary = str.split("")
+  let newAry = []
+
+  ary.forEach( (ele) =>  newAry.unshift(ele) )
+
+  return newAry.join("")
 }
