0ec0c739df681667ea9f419c6ba1552aec8ec56c
wowdyaln <rogersmk@gmail.com.tw>
2018-09-07 11:38:28 +0800

    week1-2 fixed

--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -2,11 +2,10 @@
 1. 使用 .split("") 切開
 2. 一個個檢查：如果不是英文字母就忽略
 3. 第一個字母，轉成大寫。已經碰到第一個字母 = true
-4. 第一個字母之後的字母都轉成小寫。
 */
 
 
-function isEnLetter(letter){
+function isEnLetter(letter) {
   return /[a-zA-Z]/.test(letter)
 }
 
@@ -15,17 +14,15 @@ function capitalize(str) {
   let ary = str.split("")
   let meetFirstLetter = false
 
-  let output = ary.map( (ele)=> {
-    if (meetFirstLetter && isEnLetter(ele)) {
-      return ele.toLowerCase()
-    } else if (!meetFirstLetter && isEnLetter(ele)){
+  let output = ary.map((ele) => {
+    if (!meetFirstLetter && isEnLetter(ele)) {
       meetFirstLetter = true
       return ele.toUpperCase()
     } else if (!isEnLetter(ele)) {
+      return ele
+    } else if (meetFirstLetter && isEnLetter(ele)) {
       return ele
     }
   })
   return output.join("")
 }
-
-
