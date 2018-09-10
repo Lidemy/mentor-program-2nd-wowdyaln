5976948cd2cfae4a430f021e574f438cb97cff68
wowdyaln <rogersmk@gmail.com.tw>
2018-09-10 11:01:55 +0800

    hw2-1 ~ hw2-3   done.

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,21 @@
-function stars(n) {
-
+function unit(k) {
+  let uni = ""
+  for (let j = 0; j < k; j++) {
+    uni += "*"
+  }
+  return uni
 }
+
+function stars(n) {
+  let newAry = []
+  for (let i = 1; i <= n; i++){
+    let ele = unit(i)
+    newAry.push(ele)
+  }
+  return newAry
+}
+
+a= stars(10)
+console.log(a);
 
 module.exports = stars;
--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,26 @@
-function alphaSwap(str) {
-
+/*
+1. 使用.split("") 切開
+2. 一個個檢查：如果不是英文字母就忽略
+3. 大寫轉小寫，反之亦然
+*/
+function isCapital(letter) {
+  return /[A-Z]/.test(letter)
+}
+function isLowercase(letter) {
+  return /[a-z]/.test(letter)
 }
 
+function alphaSwap(str) {
+  let ary = str.split("")
+  let output = ary.map((ele) => {
+    if (isCapital(ele) ) {
+      return ele.toLowerCase()
+    } else if ( isLowercase(ele) ) {
+      return ele.toUpperCase()
+    } else {
+      return ele
+    }
+  })
+  return output.join("")
+}
 module.exports = alphaSwap
